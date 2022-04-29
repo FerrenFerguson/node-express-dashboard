a4b276d2d2709120e97fb70746af5679014debd9
ferren ferguson <ferren.ferguson@bt.com>
2022-04-29 11:36:55 +0100

    npm run test:module5

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -4,22 +4,23 @@ const path = require("path");
 function getDirectoryContents(files, currentDir, query) {
   const data = [];
   data.forEach((files) => {
-    isDirectory(if(files == currentDir){
-      data.push({
-        name: file,
-        isDirectory: true,
-        path: path.join(query, file)
-      })
-    } else {
-      data.push({
-        name: file,
-        isDirectory: false,
-        path: path.join(query, file, currentDir)
-      })
-    })
-    return data;
+    // isDirectory(
+    //   if(files == currentDir) {
+    //   data.push({
+    //     name: file,
+    //     isDirectory: true,
+    //     path: path.join(query, file)
+    //   })else {
+    //   data.push({
+    //     name: file,
+    //     isDirectory: false,
+    //     path: path.join(query, file, currentDir)
+    //   })
+    // }
+    // })
+    // return data;
   });
-  return data;
+  // return data;
 }
 
 function isDirectory(currentDir, file) {
