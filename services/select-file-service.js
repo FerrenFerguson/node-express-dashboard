494121a3c5e0a00de6edcfbdc0419ee3a908137d
ferren ferguson <ferren.ferguson@bt.com>
2022-04-29 11:33:01 +0100

    Selecting a log file

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -1,10 +1,29 @@
 const fs = require("fs");
 const path = require("path");
 
-function getDirectoryContents(files, currentDir, query) {}
+function getDirectoryContents(files, currentDir, query) {
+  const data = [];
+  data.forEach((files) => {
+    isDirectory(if(files == currentDir){
+      data.push({
+        name: file,
+        isDirectory: true,
+        path: path.join(query, file)
+      })
+    } else {
+      data.push({
+        name: file,
+        isDirectory: false,
+        path: path.join(query, file, currentDir)
+      })
+    })
+    return data;
+  });
+  return data;
+}
 
 function isDirectory(currentDir, file) {
-  const fileInfo = fs.statSync(path.join("./"));
+  const fileInfo = fs.statSync(path.join(currentDir, file));
   return fileInfo.isDirectory();
 }
 
