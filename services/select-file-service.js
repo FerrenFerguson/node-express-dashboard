49018665a16d47ae83440972d1bcde2dd05dab55
ferren ferguson <ferren.ferguson@bt.com>
2022-04-29 10:59:49 +0100

    npm run test:module5

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -1,17 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-
-unction getDirectoryContents(files, currentDir, query) {
-}
+function getDirectoryContents(files, currentDir, query) {}
 
 function isDirectory(currentDir, file) {
-  const fileInfo = fs.statSync(path.join('./'));
+  const fileInfo = fs.statSync(path.join("./"));
   return fileInfo.isDirectory();
 }
 
-function readDir(currentDir, res, query) {
-}
+function readDir(currentDir, res, query) {}
 
-exports.get = (req, res) => {
-};
+exports.get = (req, res) => {};
