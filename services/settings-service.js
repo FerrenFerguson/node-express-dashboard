23fa2dca52e0f2ad0feb2eab0db619ac06b1cd88
ferren ferguson <ferren.ferguson@bt.com>
2022-05-03 15:49:42 +0100

    Adding settings and configuration

--- services/settings-service.js
+++ services/settings-service.js
@@ -5,14 +5,36 @@ const path = require("path");
 const settingsFilePath = path.join(__dirname, "../json/settings.json");
 
 function getSettings() {
-  return {};
+  const settingsData = fs.readFileSync(settingsFilePath);
+  return JSON.parse(settingsData);
 }
 
-function writeSettings(newSettings) {}
+function writeSettings(newSettings) {
+  const settingsJSON = JSON.stringify(newSettings, null, 2);
+  try {
+    fs.writeFileSync(settingsFilePath, settingsJSON);
+    return true;
+  } catch {
+    return false;
+  }
+}
 
-function getDefaultDir() {}
+function getDefaultDir() {
+  const defaultDir = getSettings().defaultDir;
+  if (!defaultDir) {
+    return process.cwd();
+  }
+  isValidDir(defaultDir) ? defaultDir : process.cwd();
+}
 
-function isValidDir(dirPath) {}
+function isValidDir(dirPath) {
+  try {
+    fs.readdirSync(dirPath);
+    return true;
+  } catch {
+    return false;
+  }
+}
 
 module.exports = {
   getSettings,
