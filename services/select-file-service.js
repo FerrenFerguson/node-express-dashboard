5b811060bb697ad08b444f6c01f6c2adef702506
ferren ferguson <ferren.ferguson@bt.com>
2022-05-04 10:37:09 +0100

    npm run test:module6

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -1,8 +1,11 @@
 const { json } = require("express/lib/response");
 const fs = require("fs");
 const path = require("path");
-const dir = process.cwd();
-
+let dir;
+ 
+exports.setcwd = (cwd) = { 
+  dir = cwd;
+}
 function getDirectoryContents(files, currentDir, query) {
   const data = [];
   files.forEach((file) => {
